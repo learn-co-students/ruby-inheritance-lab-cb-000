0b5202ce9d469d580a44c71187d4b514c5ec32cc
David Ordovskiy <david.ordovsky@gmail.com>
2017-02-13 16:12:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < Teacher
+
+  def initialize
+    @knowledge = []
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+  def learn(info)
+    self.knowledge << info
+  end
 
 end
