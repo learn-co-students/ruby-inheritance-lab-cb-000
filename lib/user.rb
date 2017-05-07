46c3536799e3d0d24483a5fe41f4e197a21c97da
Aaron Kelton <aaron.k.kelton@gmail.com>
2017-05-07 12:30:40 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+  attr_accessor :first_name, :last_name
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+  # Users probably aren't initialized, as they are generic.
+  # Only actual teachers and students get initialized.
+end
