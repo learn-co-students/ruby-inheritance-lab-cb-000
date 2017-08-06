9c52f62c574deb49033813aa6fe08a0a5201dcd4
Patrick Townley <patricktownley@gmail.com>
2017-08-06 15:36:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_reader :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(bit_of_knowledge)
+    @knowledge << bit_of_knowledge
+  end
 end
