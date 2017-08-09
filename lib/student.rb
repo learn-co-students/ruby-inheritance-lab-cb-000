af58e86f07979a03ccd0e84153bbf02f4a6f08c5
stritch.dev <stritch.dev@gmail.com>
2017-08-09 13:23:28 -0500

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require '/Users/jd/Documents/applicationDevelopment/flatiron/web/ruby-inheritance-lab-cb-000/lib/user.rb'
 
+class Student < User
+  @knowledge = []
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(info)
+    @knowledge << info
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
