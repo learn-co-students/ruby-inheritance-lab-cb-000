625b502d459ab0e81972044d4ca0f9580a96131b
Jen Poole <contact.vandepool@gmail.com>
2017-12-18 23:26:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,4 +5,21 @@ require_relative "./user.rb"
 # has a last name
 class Student < User
 
+  attr_accessor :knowledge
+
+  # initializes with an empty knowledge array
+  def initialize
+    @knowledge = []
+  end
+
+  # takes in an argument of a string of knowledge and adds it to the student's knowledge array
+  def learn(info)
+    @knowledge << info
+  end
+
+  # returns that student's knowledge array
+  def knowledge
+    @knowledge
+  end
+
 end
