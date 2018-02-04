85dd86bb818642a3156e9c66741f155126041279
Carl Cliver <ccliver@gmail.com>
2018-02-04 14:56:20 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student  < User
+  attr_accessor :knowledge
 
+  def initialize
+    self.knowledge = []
+    super
+  end
+
+  def learn(subject)
+    self.knowledge << subject
+  end
 end
