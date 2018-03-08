75da1de494bfdf4e11cf7299735f9cffecd5e159
adam n <adam.nawroc@gmail.com>
2018-03-08 12:55:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(ting)
+    @knowledge << ting
+
+  end
+
+  def knowledge
+    @knowledge
+
+  end
 end
