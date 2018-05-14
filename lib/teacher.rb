3d9cbd79bb4a34b156e83787b4ed4b2626d76299
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-14 08:46:35 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,15 +1,6 @@
 class Teacher < User
-
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
-<<<<<<< HEAD
   def teach
-    KNOWLEDGE[rand(KNOWLEDGE.size)]
+    KNOWLEDGE[rand(KNOWLEDGE.length)]
   end
-=======
-  def teach 
-    KNOWLEDGE[rand(KNOWLEDGE.size)]
-  end 
->>>>>>> 4bc32f5f047a529c59890cea4f9cfdd3ebd8f803
-
 end
