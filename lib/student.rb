fb0c2a05b2322d11b5e2accd5ef68e1d041730dd
marysue <mary@marylark.com>
2019-03-04 15:24:42 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledgeStr)
+        @knowledge << knowledgeStr
+    end
+
+    def knowledge
+        puts("====================================")
+        puts("#{first_name} #{last_name}'s knowledge:\n")
+        puts("Knowledge size:  #{@knowledge.size}\n")
+        @knowledge.each{ |str| puts str}
+        puts("====================================")  
+    end
 
 end
