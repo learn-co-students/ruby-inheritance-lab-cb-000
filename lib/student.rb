82b8f31ff90c61c82db41505cd285bc24aec25fc
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-05 21:07:07 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+  def learn(string_o_knowledge)
+    @knowledge << string_o_knowledge
+  end
+  def knowledge
+    @knowledge
+  end
 end
